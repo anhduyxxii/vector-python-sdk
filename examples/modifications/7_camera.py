dfcad502f0b11186baf3497669cd67d836a00892
Tran Anh Duy <andy@mmj.vn>
2019-04-24 16:18:23 +0700

    add camera feed and enhance taking picture

--- examples/modifications/7_camera.py
+++ examples/modifications/7_camera.py
@@ -20,16 +20,15 @@ Make Vector say 'Hello World' in this simple Vector SDK example program.
 """
 import time
 import anki_vector
-from anki_vector.util import degrees
+
 
 def main():
-    with anki_vector.Robot(enable_camera_feed=True) as robot:
-        print("Moving Vector's head...")
-        robot.behavior.set_head_angle(degrees(20.0))
-        time.sleep(3)
-        print("Taking picture...")
-        image = robot.camera.latest_image
-        image.show()
+    with anki_vector.Robot(enable_camera_feed=False) as robot:
+        print("Vector's showing camera feed...")
+        robot.viewer.show_video()
+        time.sleep(10)
+        robot.viewer.stop_video()
+        print("Stopped camera feed")
 
 if __name__ == "__main__":
     main()
